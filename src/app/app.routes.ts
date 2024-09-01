72953ab4a843c7cac290b0902614f9f04f2a94e7
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2024-09-01 16:18:20 +0200

    contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { Routes } from '@angular/router';
@@ -5,5 +6,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'privacy', component: PrivacyComponent },
+    { path: 'contact', component: ContactComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
